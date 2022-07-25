15eb495c2615c9772b19f679a02b97dab4c723a9
Aissa SOUBHI <asoubhi@nexity.fr>
2022-07-25 22:10:03 +0200

    Add summernoteHeading brick

--- src/Module/SubBrickLoader.js
+++ src/Module/SubBrickLoader.js
@@ -1,9 +1,11 @@
 import SummernoteGallery from "summernote-gallery/src/Module"
+import SummernoteHeading from "summernote-heading/dist/module"
 
 export default {
   loadSubBrick(subBrick) {
       switch (subBrick) {
           case 'summernote-gallery': return SummernoteGallery
+          case 'summernote-heading': return SummernoteHeading
       }
   }
 }
