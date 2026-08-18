7bb32ea08f8bd312c1cf4b6e6f131da72f1d92f3
Eissa Soubhi <eissa.soubhi@gmail.com>
2026-08-18 02:28:11 +0200

    support custom brick registrations

--- src/Module/SummernoteBricks.js
+++ src/Module/SummernoteBricks.js
@@ -1,25 +1,24 @@
 import SubBrickLoader from "./SubBrickLoader";
-import SummernoteGallery from "summernote-gallery/src/Module"
-// implements SummernotePluginInterface
+
+// Orchestrates standalone Summernote brick plugins under a single dropdown.
 export default class SummernoteBricks
 {
     constructor(options) {
         this.options = $.extend({
             name: 'summernoteBricks',
             buttonLabel: '<i class="fa fa-puzzle-piece"></i> SN bricks',
-            tooltip: 'summernote bricks Dropdown',
+            tooltip: 'Summernote bricks',
             SNOptions: {}
         }, options);
         this.plugins = []
         this.subBricks = []
+        this.subBrickLoader = new SubBrickLoader()
 
-
-        // Add the sub-bricks to the summernote plugins
+        // Add the sub-bricks to the summernote plugins.
         this.loadSubBricks()
     }
 
     createDropdown() {
-
         let components = [];
 
         for (let i = 0; i < this.subBricks.length; i++) {
@@ -27,25 +26,17 @@ export default class SummernoteBricks
         }
 
         let dropdown = $.summernote.ui.buttonGroup([
-            /* create a button */
             $.summernote.ui.button({
                 className: 'dropdown-toggle',
                 contents: this.options.buttonLabel,
                 tooltip: this.options.tooltip,
-                click: function () {
-                    console.log('summernote bricks button clicked')
-                },
                 data: {
                     toggle: 'dropdown'
                 }
             }),
-            /* create a dropdown */
             $.summernote.ui.dropdown({
                 contents: components,
-                className: 'dropdown-style',
-                callback: function($dropdown) {
-
-                }
+                className: 'dropdown-style'
             })
         ])
 
@@ -53,21 +44,22 @@ export default class SummernoteBricks
     }
 
     loadSubBricks() {
-
         let options = this.options.SNOptions[this.options.name] || {};
         let subBricks = options.subBricks || []
+        let brickTypes = options.brickTypes || {}
+
+        Object.keys(brickTypes).forEach((name) => {
+            this.subBrickLoader.register(name, brickTypes[name])
+        })
 
         $.each(subBricks, (index, subBrick) => {
-
-            let SubBrick = SubBrickLoader.loadSubBrick(subBrick)
-
+            let SubBrick = this.subBrickLoader.loadSubBrick(subBrick)
             this.subBricks.push(new SubBrick())
         })
 
         for (let i = 0; i < this.subBricks.length; i++) {
             let subBrick = this.subBricks[i]
             let plugin = subBrick.getPlugin()
-            // console.log('plugin', plugin)
             $.extend($.summernote.plugins, plugin);
         }
     }
