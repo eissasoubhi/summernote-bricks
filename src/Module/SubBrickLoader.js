4b83655cb3738f51fa0902a58f910b3cafe55f57
Eissa Soubhi <eissa.soubhi@gmail.com>
2026-08-18 02:28:03 +0200

    replace hard-coded loader with registry

--- src/Module/SubBrickLoader.js
+++ src/Module/SubBrickLoader.js
@@ -1,11 +1,25 @@
 import SummernoteGallery from "summernote-gallery/dist/module"
 import SummernoteHeading from "summernote-heading/dist/module"
+import BrickRegistry from "./BrickRegistry"
 
-export default {
-  loadSubBrick(subBrick) {
-      switch (subBrick) {
-          case 'summernote-gallery': return SummernoteGallery
-          case 'summernote-heading': return SummernoteHeading
-      }
-  }
+export default class SubBrickLoader {
+    constructor(registry) {
+        this.registry = registry || new BrickRegistry({
+            'summernote-gallery': SummernoteGallery,
+            'summernote-heading': SummernoteHeading,
+        });
+    }
+
+    register(name, Brick) {
+        this.registry.register(name, Brick);
+        return this;
+    }
+
+    loadSubBrick(name) {
+        return this.registry.resolve(name);
+    }
+
+    names() {
+        return this.registry.names();
+    }
 }
