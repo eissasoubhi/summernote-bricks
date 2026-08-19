1606ef110b40e87c7a4409e58e2171501acfd807
Eissa Soubhi <eissa.soubhi@gmail.com>
2026-08-19 15:15:22 +0200

    build(v3): emit JavaScript to root dist

--- v3-tooling/vite.config.ts
+++ v3-tooling/vite.config.ts
@@ -1,13 +1,15 @@
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
     lib: {
-      entry: new URL('./src/index.ts', import.meta.url).pathname,
+      entry: fileURLToPath(new URL('./src/index.ts', import.meta.url)),
       name: 'SummernoteBricksV3',
       formats: ['es', 'umd'],
       fileName: (format) => format === 'es' ? 'summernote-bricks.js' : 'summernote-bricks.umd.cjs',
     },
+    outDir: fileURLToPath(new URL('../dist', import.meta.url)),
     sourcemap: true,
     emptyOutDir: true,
   },
